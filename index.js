9a43bf5cd229d820f5444567e2876d79e4ece254
alikansoh <alikansoh.dev@gmail.com>
2023-09-22 10:04:55 +0300

    step 3  finished

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ server.get("/", (req, res) => {
     res.send('ok')
 })
 
+server.get("/test", (req, res) => {
+    res.json({status:200, message:"ok"})
+})
+
+server.get("/time", (req, res) => {
+    let today=new Date();
+    res.json({status:200 ,time:today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()})
+})
+
+
 server.listen(3000,()=>console.log("run server"))
