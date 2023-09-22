5744f66c35d6f4a2e396516b70889501a7f697f6
alikansoh <alikansoh.dev@gmail.com>
2023-09-22 10:41:11 +0300

    step 4 finished

--- index.js
+++ index.js
@@ -16,4 +16,21 @@ server.get("/time", (req, res) => {
 })
 
 
+server.get("/hello/:id?", (req, res) => {
+        if (!req.params.id)
+            req.params.id= " "
+    res.json({status:200, message: 'hello,' +req.params.id})
+})
+
+server.get("/search/:s?", (req, res) => {
+    if (req.params.s)
+    res.json({status:200, message: 'ok,' +req.params.s})
+    else
+        res.json({status:500, error: true,message: 'you have to provide a search'})
+})
+
+
+
+
+
 server.listen(3000,()=>console.log("run server"))
